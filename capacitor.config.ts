f903a95d901571194e75f48df34bdfc1997b0e8b
ADRIAN <adriandeligero.1030@gmail.com>
2025-05-06 21:25:02 +0800

    added capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.aidflow',
+  appName: 'aidflow',
+  webDir: 'dist'
+};
+
+export default config;
