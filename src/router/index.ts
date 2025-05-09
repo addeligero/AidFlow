84b02a5364e14dcbb54827fe3133a6d734789f26
ADRIAN <adriandeligero.1030@gmail.com>
2025-05-09 11:49:17 +0800

    section: added dashboard

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import register from '@/views/Auth/RegisterForm.vue'
+import Dashboard from '@/views/DashboardSection.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/register',
       name: 'register',
       component: register,
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: Dashboard,
     },
   ],
 })
