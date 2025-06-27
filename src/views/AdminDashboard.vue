f0df25508ea7dcc977caa04988b9da8ec4a9f0d1
ADRIAN <adriandeligero.1030@gmail.com>
2025-06-27 20:32:04 +0800

    admin section: implemented header and sidebar

--- src/views/AdminDashboard.vue
+++ src/views/AdminDashboard.vue
@@ -1,6 +1,37 @@
-<script setup>
-import Dashboard from '../components/Admin/Dashboard.vue'
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import Sidebar from '@/components/Client dashboard/SidebarContainer.vue'
+import Header from '@/components/HeaderSection.vue'
+
+import { userCounterStore } from '@/stores/users'
+
+const drawer = ref(false)
+const toggleDrawer = () => {
+  drawer.value = !drawer.value
+}
+
+const userStore = userCounterStore()
+
+onMounted(async () => {
+  await userStore.fetchUsers()
+})
 </script>
+
 <template>
-  <Dashboard />
+  <Sidebar v-model="drawer" />
+
+  <Header :toggleDrawer="toggleDrawer" />
 </template>
+
+<style scoped>
+.main-content {
+  margin-top: 20px;
+}
+
+.content-wrapper {
+  background-color: white;
+  padding: 1rem;
+  border-radius: 8px;
+  box-shadow: 0 4px 20px rgba(0, 0, 0, 0.1);
+}
+</style>
