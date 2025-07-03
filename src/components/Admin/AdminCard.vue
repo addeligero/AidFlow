a90759d3d7be4fa32da47a2c2dd0752df6cb55f6
ADRIAN <adriandeligero.1030@gmail.com>
2025-07-03 10:57:34 +0800

    feat: made the card dynamic

--- src/components/Admin/AdminCard.vue
+++ src/components/Admin/AdminCard.vue
@@ -1,22 +1,32 @@
 <script setup lang="ts">
 import image from '@/assets/img/admin/rules.png'
+
+const props = defineProps<{
+  title?: string
+  subHeader?: string
+  shortStatement?: string
+  buttonText?: string
+}>()
+
+const title = props.title ?? 'title'
+const subHeader = props.subHeader ?? 'Headings'
+const shortStatement = props.shortStatement ?? 'Short Statement'
+const buttonText = props.buttonText ?? 'Button Text'
 </script>
 <template>
   <v-card class="mx-auto" max-width="400">
     <v-img class="align-end text-white" height="200" :src="image">
-      <v-card-title>Rules</v-card-title>
+      <v-card-title>{{ title }}</v-card-title>
     </v-img>
 
-    <v-card-subtitle class="pt-4"> Number 10 </v-card-subtitle>
+    <v-card-subtitle class="pt-4"> {{ subHeader }}</v-card-subtitle>
 
     <v-card-text>
-      <div>Whitehaven Beach</div>
-
-      <div>Whitsunday Island, Whitsunday Islands</div>
+      <div>{{ shortStatement }}</div>
     </v-card-text>
 
     <v-card-actions>
-      <v-btn color="orange" text="Share"></v-btn>
+      <v-btn color="orange" :text="buttonText"></v-btn>
 
       <v-btn color="orange" text="Explore"></v-btn>
     </v-card-actions>
