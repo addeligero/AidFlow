d9279867e622997c4710d323a9da6eb941fe2ba5
ADRIAN <adriandeligero.1030@gmail.com>
2025-08-12 21:15:59 +0800

    initial commit

--- src/components/Client dashboard/CarouselSection.vue
+++ src/components/Client dashboard/CarouselSection.vue
@@ -1,32 +1,38 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+
 import Philhealth from '@/assets/img/logo/philhealth.jpg'
 import DOH from '@/assets/img/logo/doh.png'
 import DSWD from '@/assets/img/logo/DSWD.SVG.png'
 import MALASAKIT from '@/assets/img/logo/malasakit.jpg'
-import LogoImage from '@/components/Client dashboard/LogoImage.vue'
 import pcso from '@/assets/img/logo/pcso.jpg'
 
+const model = ref(null)
 const images = [Philhealth, DOH, DSWD, MALASAKIT, pcso]
 </script>
 
 <template>
-  <v-sheet class="mx-auto" elevation="8" max-width="800">
-    <v-slide-group v-model="model" class="pa-4" selected-class="bg-success" show-arrows>
-      <v-slide-group-item
-        v-for="(img, i) in images"
-        :key="i"
-        v-slot="{ isSelected, toggle, selectedClass }"
-      >
-        <v-card
-          :class="['ma-4', selectedClass]"
-          color="grey-lighten-1"
-          height="90"
-          width="150"
-          @click="toggle"
-        >
+  <v-sheet class="mx-auto" elevation="8" max-width="800" color="transparent">
+    <v-slide-group v-model="model" selected-class="bg-success">
+      <v-slide-group-item v-for="(img, i) in images" :key="i" v-slot="{ toggle, selectedClass }">
+        <v-card :class="['ma-4', selectedClass]" height="150" width="250" @click="toggle">
           <div class="d-flex fill-height align-center justify-center">
             <v-scale-transition>
-              <v-img :src="img"></v-img>
+              <v-card color="transparent">
+                <div class="d-flex align-center">
+                  <v-img :src="img" height="90" width="90" cover class="ma-3"></v-img>
+
+                  <div>
+                    <v-card-title>Top western road trips</v-card-title>
+                    <v-card-subtitle>1,000 miles of wonder</v-card-subtitle>
+                  </div>
+                </div>
+
+                <v-card-actions>
+                  <v-btn color="orange-lighten-2" text="Explore"></v-btn>
+                  <v-spacer></v-spacer>
+                </v-card-actions>
+              </v-card>
             </v-scale-transition>
           </div>
         </v-card>
