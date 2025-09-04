d33d231190bc8cf255c1b32cead06f1c5f138dc7
ADRIAN <adriandeligero.1030@gmail.com>
2025-09-04 10:19:28 +0800

    feat: added chart js for better checking status for admin

--- src/views/admin/AdminDashboard.vue
+++ src/views/admin/AdminDashboard.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import AdminLayout from '@/layouts/AdminLayout.vue'
 import AdminCard from '@/components/Admin/AdminCard.vue'
+import DashboardChart from '@/views/admin/DashboardChart.vue'
 </script>
 <template>
   <AdminLayout>
+    <DashboardChart />
     <AdminCard
       title="Rules"
       subHeader="Empower Your Community"
